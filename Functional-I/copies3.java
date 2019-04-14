d24828eacf43416e6b0fa9ef77e833771f8a9b4e
vennela28 <vennela.jobs@gmail.com>
2019-04-14 18:46:39 +0530

    Functional-I updates

--- Functional-I/copies3.java
+++ Functional-I/copies3.java
@@ -2,3 +2,9 @@ public List<String> copies3(List<String> strings) {
   strings.replaceAll(s -> s + s + s);
   return strings;
 }
+
+//*****************************OR*******************************************
+
+public List<String> copies3(List<String> strings) {
+  return strings.stream().map(s -> s + s + s).collect(Collectors.toList());
+}
--- Functional-I/math1.java
+++ Functional-I/math1.java
@@ -5,3 +5,6 @@ public List<Integer> math1(List<Integer> nums) {
 
 //*****************************OR*******************************************
 
+public List<Integer> math1(List<Integer> nums) {
+  return nums.stream().map(n -> (n + 1) * 10).collect(Collectors.toList());
+}
